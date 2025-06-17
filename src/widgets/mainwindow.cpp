94065765cc7b65d62b385afe5ab90c75d4603e00
Александр Д. Терентьев <Terentev@nanoscopy.org>
2025-06-18 01:10:09 +0300

    Fix bug with steps

--- src/widgets/mainwindow.cpp
+++ src/widgets/mainwindow.cpp
@@ -9,28 +9,36 @@ MainWindow::MainWindow(QWidget *parent)
     , ui{new MainWindow_Ui{this}}
 {
     ui->knobWidget->setButton(TileWidget::Place::TopLeft, ui->tileWidgetButtonNames[TileWidget::Place::TopLeft], [this]() -> void {
-        ArduinoWithSteppers::instance().manualCalibrate();
+        if (ArduinoWithSteppers::instance().isConnected() && ui->distanceValueWidget->distance() > std::numeric_limits<double>::epsilon())
+            ArduinoWithSteppers::instance().manualCalibrate();
     });
     ui->knobWidget->setButton(TileWidget::Place::Top, ui->tileWidgetButtonNames[TileWidget::Place::Top], [this]() -> void {
-        ArduinoWithSteppers::instance().move(ui->distanceValueWidget->distance(), ArduinoWithSteppers::Up);
+        if (ArduinoWithSteppers::instance().isConnected() && ui->distanceValueWidget->distance() > std::numeric_limits<double>::epsilon())
+            ArduinoWithSteppers::instance().move(ui->distanceValueWidget->distance(), ArduinoWithSteppers::Up);
     });
     ui->knobWidget->setButton(TileWidget::Place::Left, ui->tileWidgetButtonNames[TileWidget::Place::Left], [this]() -> void {
-        ArduinoWithSteppers::instance().move(ui->distanceValueWidget->distance(), ArduinoWithSteppers::Right);
+        if (ArduinoWithSteppers::instance().isConnected() && ui->distanceValueWidget->distance() > std::numeric_limits<double>::epsilon())
+            ArduinoWithSteppers::instance().move(ui->distanceValueWidget->distance(), ArduinoWithSteppers::Right);
     });
     ui->knobWidget->setButton(TileWidget::Place::Central, ui->tileWidgetButtonNames[TileWidget::Place::Central], [this]() -> void {
-        ArduinoWithSteppers::instance().moveToCenter();
+        if (ArduinoWithSteppers::instance().isConnected())
+            ArduinoWithSteppers::instance().moveToCenter();
     });
     ui->knobWidget->setButton(TileWidget::Place::Right, ui->tileWidgetButtonNames[TileWidget::Place::Right], [this]() -> void {
-        ArduinoWithSteppers::instance().move(ui->distanceValueWidget->distance(), ArduinoWithSteppers::Left);
+        if (ArduinoWithSteppers::instance().isConnected() && ui->distanceValueWidget->distance() > std::numeric_limits<double>::epsilon())
+            ArduinoWithSteppers::instance().move(ui->distanceValueWidget->distance(), ArduinoWithSteppers::Left);
     });
     ui->knobWidget->setButton(TileWidget::Place::BottomLeft, ui->tileWidgetButtonNames[TileWidget::Place::BottomLeft], [this]() -> void {
-        ArduinoWithSteppers::instance().autoCalibrate();
+        if (ArduinoWithSteppers::instance().isConnected() && ui->distanceValueWidget->distance() > std::numeric_limits<double>::epsilon())
+            ArduinoWithSteppers::instance().autoCalibrate();
     });
     ui->knobWidget->setButton(TileWidget::Place::Bottom, ui->tileWidgetButtonNames[TileWidget::Place::Bottom], [this]() -> void {
-        ArduinoWithSteppers::instance().move(ui->distanceValueWidget->distance(), ArduinoWithSteppers::Down);
+        if (ArduinoWithSteppers::instance().isConnected() && ui->distanceValueWidget->distance() > std::numeric_limits<double>::epsilon())
+            ArduinoWithSteppers::instance().move(ui->distanceValueWidget->distance(), ArduinoWithSteppers::Down);
     });
     ui->knobWidget->setButton(TileWidget::Place::BottomRight, ui->tileWidgetButtonNames[TileWidget::Place::BottomRight], [this]() -> void {
-        ArduinoWithSteppers::instance().stop();
+        if (ArduinoWithSteppers::instance().isConnected())
+            ArduinoWithSteppers::instance().stop();
     });
 
     connect(
